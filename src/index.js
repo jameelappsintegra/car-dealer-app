9319f1c55646924dcb2149ea668bbd4d00e01d21
jameelappsintegra <47351791+jameelappsintegra@users.noreply.github.com>
2020-07-10 20:04:43 +0400

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,38 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { createBrowserHistory } from "history";
+import { Router, Route, Switch } from "react-router-dom";
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
+
+import "assets/scss/material-kit-react.scss?v=1.9.0";
+
+// pages for this product
+// import Components from "views/Components/Components.js";
+import LandingPage from "views/LandingPage/LandingPage.js";
+import ProfilePage from "views/ProfilePage/ProfilePage.js";
+import LoginPage from "views/LoginPage/LoginPage.js";
+
+var hist = createBrowserHistory();
+const client = new ApolloClient({
+  uri: 'https://evident-rooster-66.hasura.app/v1/graphql',
+  headers: {
+    "content-type": "application/json",
+    "x-hasura-admin-secret": "eaeabtcoqbkmhbmqkxmydcjtxwkltfok"
+  }
+});
+
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+  <Router history={hist}>
+    <Switch>
+      <Route path="/landing-page" component={LandingPage} />
+      <Route path="/profile-page" component={ProfilePage} />
+      <Route path="/login-page" component={LoginPage} />
+      <Route path="/" component={LoginPage} />
+    </Switch>
+  </Router>
+  </ApolloProvider>,
+  document.getElementById("root")
+);
