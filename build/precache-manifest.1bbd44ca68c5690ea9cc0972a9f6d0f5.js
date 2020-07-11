c86ddb1dbfd115165aeb1164f23e6bbdc3aec10a
Jameel <jameel.ahamed@appsintegra.com>
2020-07-12 00:42:43 +0400

    build commit

---
+++ build/precache-manifest.1bbd44ca68c5690ea9cc0972a9f6d0f5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ef9bd062c9c6069dba298ca606ecb51",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a6a775cf9e78cc6424a7",
+    "url": "/static/css/main.126c14d6.chunk.css"
+  },
+  {
+    "revision": "693ceb1187293d2c0910",
+    "url": "/static/js/2.9be49753.chunk.js"
+  },
+  {
+    "revision": "0b474006a00d74694d274dca100d01ef",
+    "url": "/static/js/2.9be49753.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6a775cf9e78cc6424a7",
+    "url": "/static/js/main.47db8f3d.chunk.js"
+  },
+  {
+    "revision": "948fd2822e92ef0377f6",
+    "url": "/static/js/runtime-main.c86b6328.js"
+  },
+  {
+    "revision": "b08234a2b7e8e4fb2e3c4e713252021c",
+    "url": "/static/media/bg7.b08234a2.jpg"
+  },
+  {
+    "revision": "df8fd3efcd662b64b44de07f351c838b",
+    "url": "/static/media/landing-bg.df8fd3ef.jpg"
+  }
+]);
